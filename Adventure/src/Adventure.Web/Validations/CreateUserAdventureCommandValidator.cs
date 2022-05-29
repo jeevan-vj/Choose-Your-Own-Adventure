4d64e173fd66fc8910896593a33eb8c10926ffcb
jeevan <jeevan90wijerathna@gmail.com>
2022-05-29 23:57:17 +0800

    CreateUserAdventureCommandValidatorTests added

---
+++ Adventure/src/Adventure.Web/Validations/CreateUserAdventureCommandValidator.cs
@@ -0,0 +1,62 @@
+﻿using Adventure.Core.Models;
+using Adventure.Web.Commands;
+using FluentValidation;
+
+namespace Adventure.Web.Validations;
+
+public class CreateUserAdventureCommandValidator : AbstractValidator<CreateUserAdventureCommand>
+{
+  public CreateUserAdventureCommandValidator()
+  {
+    RuleFor(command => command.Adventure).NotNull().WithMessage("No Adventure Object found");
+    RuleFor(command => command.UserId).NotNull().GreaterThan(0).WithMessage("User Id is required to create my adventures.");
+    RuleFor(command => command.Adventure.Title).NotEmpty().WithMessage("Title is required");
+    RuleFor(command => command.Adventure.Choice).NotNull().WithMessage("No Choice found");
+    RuleFor(command => command.Adventure.Choice.SelectedOptionId).NotNull().WithMessage("A Option should be selected");
+    RuleFor(command => command.Adventure.Choice).Must(IsValid)
+      .WithMessage("Invalid data selection.Please select correct option for Choice.SelectedOptionId");
+  }
+
+  private bool IsValid(Choice choice)
+  {
+    var isValid = true;
+    var selectedOption = choice?.Options?.Where(o => o.Id == choice.SelectedOptionId).FirstOrDefault();
+    if (selectedOption == null)
+    {
+      return false;
+    }
+
+    while (true)
+    {
+      if (selectedOption != null)
+      {
+        isValid = IsValidChoice(selectedOption.Next);
+        if (!isValid)
+        {
+          break;
+        }
+      }
+
+      if (string.IsNullOrEmpty(selectedOption.Next.SelectedOptionId))
+      {
+        break;
+      }
+
+
+      selectedOption = selectedOption?.Next?.Options.First(o => o.Id == selectedOption.Next.SelectedOptionId);
+    }
+
+    return isValid;
+  }
+
+  private static bool IsValidChoice(Choice choice)
+  {
+    if ((string.IsNullOrEmpty(choice.SelectedOptionId) && choice.Options == null) || choice.Options?.Count == 0)
+    {
+      return true;
+    }
+
+    return string.IsNullOrEmpty(choice.SelectedOptionId) || choice.Options.Any(c =>
+      string.Equals(c.Id, choice.SelectedOptionId, StringComparison.Ordinal));
+  }
+}
---
+++ Adventure/tests/Adventure.UnitTests/Web/Handlers/CreateUserAdventureCommandValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Adventure.Core.Models;
+using Adventure.Web.ApiModels;
+using Adventure.Web.Commands;
+using Adventure.Web.Validations;
+using FluentAssertions;
+using Xunit;
+
+namespace Adventure.UnitTests.Web.Handlers;
+
+public class CreateUserAdventureCommandValidatorTests
+{
+  [Fact]
+  public void validate_return_errors_when_no_selected_option_in_choice()
+  {
+    var validator = new CreateUserAdventureCommandValidator();
+    var adventure = new AdventureDto
+    {
+      Title = "Adventure 1",
+      Choice = new Choice("ch1", "1") {Options = new List<Option> {new("o2", new Choice("ch2", "2"), "1")}}
+    };
+    var validationResult = validator.Validate(new CreateUserAdventureCommand(){Adventure = adventure, UserId = 1});
+    validationResult.Errors.Should().NotBeEmpty();
+  }
+}
