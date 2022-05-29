3016d4f3b1d3c804ec66fe70abb1b33c5a45b236
jeevan <jeevan90wijerathna@gmail.com>
2022-05-29 18:58:19 +0800

    Adventure Item Domain Model cleanup

--- Adventure/src/Adventure.Core/Models/AdventureItem.cs
+++ Adventure/src/Adventure.Core/Models/AdventureItem.cs
@@ -1,23 +1,22 @@
 ﻿using Adventure.SharedKernel.Interfaces;
-using Adventure.SharedKernel.Interfaces;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Adventure.Core.Models;
 
-public class AdventureItem :IAggregateRoot
+public class AdventureItem : IAggregateRoot
 {
-  [BsonId]
-  [BsonRepresentation(BsonType.ObjectId)]
-  public string? Id { get; set; }
-
   public string Name { get; set; } = string.Empty;
   public string Title { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
   public Choice Choice { get; set; }
+
+  [BsonId]
+  [BsonRepresentation(BsonType.ObjectId)]
+  public string? Id { get; set; }
 }
 
-public class Choice 
+public class Choice
 {
   public Choice(string title, string? id = null)
   {
@@ -28,24 +27,26 @@ public class Choice
   [BsonId]
   [BsonRepresentation(BsonType.ObjectId)]
   public string? Id { get; set; }
-  
+
   public string? SelectedOptionId { get; set; }
   public string Title { get; set; } = string.Empty;
 
   public List<Option>? Options { get; set; }
 }
 
-public class Option 
+public class Option
 {
-  public Option(string title,  Choice next, string? id = null)
+  public Option(string title, Choice next, string? id = null)
   {
     Title = title;
     Next = next;
     Id = id ?? ObjectId.GenerateNewId().ToString();
   }
+
   [BsonId]
   [BsonRepresentation(BsonType.ObjectId)]
   public string? Id { get; set; }
+
   public string Title { get; set; }
   public Choice? Next { get; set; }
 }
