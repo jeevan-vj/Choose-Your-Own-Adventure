d59415e538a0d897488aa7fb3a37d8b74274b8fe
jeevan <jeevan90wijerathna@gmail.com>
2022-05-29 19:16:17 +0800

    Adventure Automapper Profile Added

---
+++ Adventure/src/Adventure.Web/MappingProfiles/AdventureProfile.cs
@@ -0,0 +1,20 @@
+﻿using Adventure.Core.Models;
+using Adventure.Web.ApiModels;
+using AutoMapper;
+
+namespace Adventure.Web.MappingProfiles;
+
+public class AdventureProfile : Profile
+{
+  public AdventureProfile()
+  {
+    CreateMap<AdventureItem, AdventureDto>();
+    CreateMap<AdventureDto, AdventureItem>();
+    CreateMap<AdventureDto, UserAdventureItem>();
+    CreateMap<UserAdventureItem, UserAdventureDto>();
+    CreateMap<UserAdventureDto, UserAdventureItem>();
+    CreateMap<CreateAdventureDto, AdventureItem>();
+    CreateMap<CreateChoiceDto, Choice>();
+    CreateMap<CreateOptionDto, Option>();
+  }
+}
