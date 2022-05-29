73e39e4f91c2a8862ac580432947abe7595b267b
jeevan <jeevan90wijerathna@gmail.com>
2022-05-29 20:50:55 +0800

    Api controllers Added

---
+++ Adventure/src/Adventure.Web/Api/AdventureController.cs
@@ -0,0 +1,85 @@
+﻿using Adventure.Web.Commands;
+using Adventure.Web.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adventure.Web.Api;
+
+public class AdventureController : BaseApiController
+{
+  private readonly IMediator _mediator;
+
+  public AdventureController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpGet]
+  [Route("{adventureId}")]
+  public async Task<IActionResult> Get(string adventureId)
+  {
+    var query = new GetAdventureByIdQuery(adventureId);
+    var result = await _mediator.Send(query);
+
+    return Ok(result);
+  }
+
+  [HttpGet]
+  [Route("list-all")]
+  //[Authorize]
+  public async Task<IActionResult> List()
+  {
+    var query = new GetAllAdventuresQuery();
+    var result = await _mediator.Send(query);
+
+    return Ok(result);
+  }
+
+  [HttpPost]
+  [Route("create-adventure")]
+  public async Task<IActionResult> Post([FromBody] CreateAdventureCommand request)
+  {
+    var commandResult = await _mediator.Send(request);
+    return Ok(commandResult);
+  }
+
+  [HttpPut]
+  [Route("update-adventure")]
+  public async Task<IActionResult> Put([FromBody] UpdateAdventureCommand request)
+  {
+    var commandResult = await _mediator.Send(request);
+    return Ok(commandResult);
+  }
+
+  [HttpPost]
+  [Route("take-adventure")]
+  //[Authorize]
+  public async Task<IActionResult> Post([FromBody] CreateUserAdventureCommand request)
+  {
+    request.UserId = 1; //GetUserId();
+    var commandResult = await _mediator.Send(request);
+    return Ok(commandResult);
+  }
+
+  [HttpGet]
+  [Route("my-adventures")]
+  //[Authorize]
+  public async Task<IActionResult> Get()
+  {
+    var query = new GetMyAdventuresQuery(1);
+    var result = await _mediator.Send(query);
+
+    return Ok(result);
+  }
+
+  [HttpGet]
+  [Route("my-adventure/{adventureId}")]
+  //[Authorize]
+  public async Task<IActionResult> GetMyAdventureById(string adventureId)
+  {
+    var query = new GetMyAdventureByIdQuery(adventureId);
+    var result = await _mediator.Send(query);
+
+    return Ok(result);
+  }
+}
---
+++ Adventure/src/Adventure.Web/Api/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Adven.Web.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Adventure.Web.Api;
+
+public class AuthController : BaseApiController
+{
+  [HttpPost]
+  [Route("login")]
+  public IActionResult Login(LoginDto model)
+  {
+    if (model == null)
+    {
+      return BadRequest("Invalid client request");
+    }
+
+    if (model.UserName == "advenuser" && model.Password == "SuperStrongPassword")
+    {
+      var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@2410"));
+      var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+      var tokenOptions = new JwtSecurityToken(
+        claims: new List<Claim> {new("UserId", "1")},
+        expires: DateTime.Now.AddMinutes(50),
+        signingCredentials: signinCredentials
+      );
+
+      var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+      return Ok(new {Token = tokenString});
+    }
+
+    return Unauthorized();
+  }
+}
