eabc3d7b04aedca05ac718782aaed9a64083748e
jeevan <jeevan90wijerathna@gmail.com>
2022-05-29 18:58:43 +0800

    Adventure Domain Exception

---
+++ Adventure/src/Adventure.Core/Exceptions/AdventureDomainException.cs
@@ -0,0 +1,18 @@
+﻿namespace Adventure.Core.Exceptions;
+
+public class AdventureDomainException : Exception
+{
+  public AdventureDomainException()
+  {
+  }
+
+  public AdventureDomainException(string message)
+    : base(message)
+  {
+  }
+
+  public AdventureDomainException(string message, Exception innerException)
+    : base(message, innerException)
+  {
+  }
+}
